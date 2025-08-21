7310772ee40290ec321df22a6ffd1156df354cad
edmihaleff <ed.mihaleff@ya.ru>
2025-08-21 13:07:03 +0300

    feat: Добавил глобальных стилей. Добавил фичу календаря. Конфиг притра

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { UiCalendar } from './features';
+import AppPage from './shared/ui/AppPage.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+	<AppPage>
+		<UiCard>
+			<UiCalendar />
+		</UiCard>
+	</AppPage>
 </template>
 
 <style scoped></style>
---
+++ src/shared/ui/AppPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="app-page">
+		<slot />
+	</div>
+</template>
+
+<style>
+.app-page {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	box-sizing: border-box;
+}
+</style>
